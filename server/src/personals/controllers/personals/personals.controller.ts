f5b5e379bafc103f8d292520ebfc771ac908c16d
diary <dr.andriamampitasoa@gmail.com>
2021-11-20 16:25:17 +0100

    creation nouveau personnel

--- server/src/personals/controllers/personals/personals.controller.ts
+++ server/src/personals/controllers/personals/personals.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, InternalServerErrorException, Post } from '@nestjs/common';
+import { CreatePersonalsDto } from 'src/dto/create/create-personals.dto';
+import { PersonalsService } from 'src/personals/services/personals/personals.service';
 
 @Controller('personals')
-export class PersonalsController {}
+export class PersonalsController {
+
+    constructor(private personalService: PersonalsService) { }
+
+    @Post()
+    public create(@Body() createPersonal: CreatePersonalsDto) {
+        try {
+            return this.personalService.create(createPersonal);
+        } catch (error) {
+            throw new InternalServerErrorException();
+        }
+    }
+}
--- server/src/personals/services/personals/personals.service.ts
+++ server/src/personals/services/personals/personals.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreatePersonalsDto } from 'src/dto/create/create-personals.dto';
+import { PersonalDocument, Personals } from 'src/personals/personals.model';
 
 @Injectable()
-export class PersonalsService {}
+export class PersonalsService {
+
+    constructor(@InjectModel(Personals.name) private createPersonal: Model<PersonalDocument>) { }
+
+    public create(createPersonal: CreatePersonalsDto) {
+        try {
+            const newPersonal = new this.createPersonal(createPersonal);
+            return newPersonal.save();
+        } catch (error) {
+            throw new InternalServerErrorException();
+        }
+    }
+
+}
